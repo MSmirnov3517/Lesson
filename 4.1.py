a156ed08ef30e1a3c8a1e8362cd9156749e952ca
MSmirnov3517 <kroligne@gmail.com>
2021-01-27 19:39:56 +0300

    Done1

--- 4.1.py
+++ 4.1.py
@@ -4,4 +4,4 @@ from sys import argv
 script_name, hours, rate_per_hour, premium = argv
 pay = int(hours) * int(rate_per_hour) + int(premium)
 
-print("Зарплата работника : ", pay)
+print("Зарплата работника: ", pay)
