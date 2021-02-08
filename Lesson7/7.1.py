09abc99116087c6d971974208c19f4566f60f25e
MSmirnov3517 <kroligne@gmail.com>
2021-02-08 14:38:06 +0300

    done

--- Lesson7/7.1.py
+++ Lesson7/7.1.py
@@ -2,7 +2,6 @@ class Matrix:
     def __init__(self, a):
         self.a = a
 
-
     def __add__(self, other):
         new_a = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         for x in range(3):
@@ -15,11 +14,11 @@ class Matrix:
 
 
 m_1 = Matrix([[1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]])
+              [4, 5, 6],
+              [7, 8, 9]])
 m_2 = Matrix([[2, 3, 4],
-             [5, 6, 7],
-             [7, 8, 9]])
+              [5, 6, 7],
+              [7, 8, 9]])
 
 print(m_1.__str__())
-print(m_1+m_2)
+print(m_1 + m_2)
